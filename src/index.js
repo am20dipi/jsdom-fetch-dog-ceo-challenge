8c7fca31df8414996b31bd2cd4d48d01cf67219f
Adriana <am20dipi@alum.siena.edu>
2021-05-15 16:21:20 -0400

    commit tree f1e105bba66ff6fa5777828063147bc13a476efc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded,", function() => {
+    fetchImages();
+})
+
+function fetchImages() {
+    return fetch("https://dog.ceo/api/breeds/image/random/4" )
+    .then(response => response.json())
+    .then(json => renderImage(json))
+}
+
+function renderImages(images) {
+    const main = document.querySelector('main');
+    images.forEach(image => {
+        main.appendChild(image)
+    });
+}
